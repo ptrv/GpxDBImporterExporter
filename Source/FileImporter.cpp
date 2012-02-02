a06875d3d6355ea5d765c4f16ff29dbe9146df72
ptrv <mail@petervasil.net>
2012-02-03 00:46:25 +0100

    Fix status text in FileImporter.

--- Source/FileImporter.cpp
+++ Source/FileImporter.cpp
@@ -20,13 +20,13 @@ m_checkDuplicate(checkDuplicate)
 
 FileImporter::FileImporter(const String& dbPath, const std::vector<File>& xmlFiles, int user, bool checkMd5, bool checkDuplicate)
 :
-ThreadWithProgressWindow("GPX Import", true, false),
-m_xmlFile(String::empty),
-m_xmlFiles(xmlFiles),
-m_dbPath(dbPath),
-m_user(user),
-m_checkMd5(checkMd5),
-m_checkDuplicate(checkDuplicate)
+		ThreadWithProgressWindow("GPX Import", true, false),
+		m_xmlFile(String::empty),
+		m_xmlFiles(xmlFiles),
+		m_dbPath(dbPath),
+		m_user(user),
+		m_checkMd5(checkMd5),
+		m_checkDuplicate(checkDuplicate)
 {
 }
 
@@ -46,127 +46,132 @@ const String& FileImporter::getStatus() const
 
 void FileImporter::run()
 {
-    for(unsigned int fileNum = 0; fileNum < m_xmlFiles.size(); ++fileNum)
-    {
-	XmlDocument* xmlDoc = new XmlDocument(m_xmlFiles[fileNum]);
-	XmlElement* xmlRootElem = xmlDoc->getDocumentElement();
-	setProgress(0.0);
-	if (xmlRootElem != NULL)
+	int countGpxPoints = 0;
+	int countGpxFiles = 0;
+	m_status = String::empty;
+
+	for(unsigned int fileNum = 0; fileNum < m_xmlFiles.size(); ++fileNum)
 	{
-		std::vector<GpsData> gpsDataVec;
-		DBConnector* dbCon = new DBConnector(m_dbPath);
-		dbCon->setupDbConnection();
-		int lastFileId = -1;
-		if (!dbCon->getLastId(lastFileId, "file"))
+		XmlDocument* xmlDoc = new XmlDocument(m_xmlFiles[fileNum]);
+		XmlElement* xmlRootElem = xmlDoc->getDocumentElement();
+		setProgress(0.0);
+		if (xmlRootElem != NULL)
 		{
-			std::cout << "lastFileId = " << lastFileId << std::endl;
-		}
-		++lastFileId;
-		MD5 md5hash(m_xmlFiles[fileNum]);
-		DBG_VAL(md5hash.toHexString());
-		bool res;
-		DBG_VAL(m_checkMd5);
-		if (m_checkMd5)
-		{
-			res = dbCon->checkIfFileExists(md5hash.toHexString());
-		}
-		else
-		{
-			res = false;
-		}
-		if (!res)
-		{
-			dbCon->insertFileName(lastFileId, m_xmlFiles[fileNum].getFileName(), md5hash.toHexString());
-
-			int progressNumMax = 0;
-			int numTrk = 0;
-
-			setStatusMessage("Parsing XML file...");
-			forEachXmlChildElementWithTagName(*xmlRootElem, child, "trk")
+			std::vector<GpsData> gpsDataVec;
+			DBConnector* dbCon = new DBConnector(m_dbPath);
+			dbCon->setupDbConnection();
+			int lastFileId = -1;
+			if (!dbCon->getLastId(lastFileId, "file"))
 			{
-				forEachXmlChildElementWithTagName(*child, subchild, "trkseg")
-				{
-					forEachXmlChildElementWithTagName(*subchild, subsubchild, "trkpt")
-					{
-						++progressNumMax;
-					}
-				}
+				std::cout << "lastFileId = " << lastFileId << std::endl;
 			}
-			double progressNum = 1.0/progressNumMax;
-			int gpsdataid = -1;
-			dbCon->getLastId(gpsdataid, "gpsdata");
-			int segment = 0;
-			dbCon->getLastSegmentId(segment, m_user);
-			DBG_VAL(segment);
-			// -----------------------------------------------------------------------------
-			// get locations from database
-			// -----------------------------------------------------------------------------
-			std::vector<GpsLocation> locations;
-			dbCon->getGpsLocations(locations);
-			// -----------------------------------------------------------------------------
-			forEachXmlChildElementWithTagName(*xmlRootElem, child, "trk")
+			++lastFileId;
+			MD5 md5hash(m_xmlFiles[fileNum]);
+			DBG_VAL(md5hash.toHexString());
+			bool res;
+			DBG_VAL(m_checkMd5);
+			if (m_checkMd5)
 			{
-				forEachXmlChildElementWithTagName(*child, subchild, "trkseg")
-				{
-					++segment;
-					forEachXmlChildElementWithTagName(*subchild, subsubchild, "trkpt")
-					{
-						GpsData gpsData;
-
-						++gpsdataid;
-
-						double lat = subsubchild->getDoubleAttribute("lat");
-						double lon = subsubchild->getDoubleAttribute("lon");
-						double ele = (subsubchild->getChildElementAllSubText("ele", "0.0")).getDoubleValue();
-						String trkpttime = subsubchild->getChildElementAllSubText("time", String::empty);
-						++numTrk;
-						int loc = Helper::findLocation(locations, lon, lat);
-
-						gpsData.setGpsData(gpsdataid,
-										   (float)lat,
-										   (float)lon,
-										   trkpttime,
-										   ele,
-										   segment,
-										   m_user,
-										   lastFileId,
-										   (loc == 0 ? 1: loc));
-
-						gpsDataVec.push_back(gpsData);
-
-						setProgress(progressNum * numTrk);
-					}
-				}
+				res = dbCon->checkIfFileExists(md5hash.toHexString());
 			}
-			dbCon->beginTransaction();
-			setStatusMessage("Inserting into database...");
-			setProgress(0.0);
-			int gpsVecSize = gpsDataVec.size();
-			for (int i = 0; i < gpsVecSize; ++i) {
-				if (m_checkDuplicate)
+			else
+			{
+				res = false;
+			}
+			if (!res)
+			{
+				++countGpxFiles;
+				dbCon->insertFileName(lastFileId, m_xmlFiles[fileNum].getFileName(), md5hash.toHexString());
+
+				int progressNumMax = 0;
+				int numTrk = 0;
+
+				setStatusMessage("Parsing XML file...");
+				forEachXmlChildElementWithTagName(*xmlRootElem, child, "trk")
 				{
-					if (!dbCon->checkIfGpsDataExsist(gpsDataVec[i].getTime(), gpsDataVec[i].getUser()))
+					forEachXmlChildElementWithTagName(*child, subchild, "trkseg")
+						{
+						forEachXmlChildElementWithTagName(*subchild, subsubchild, "trkpt")
+							{
+							++progressNumMax;
+							}
+						}
+				}
+				double progressNum = 1.0/progressNumMax;
+				int gpsdataid = -1;
+				dbCon->getLastId(gpsdataid, "gpsdata");
+				int segment = 0;
+				dbCon->getLastSegmentId(segment, m_user);
+				DBG_VAL(segment);
+				// -----------------------------------------------------------------------------
+				// get locations from database
+				// -----------------------------------------------------------------------------
+				std::vector<GpsLocation> locations;
+				dbCon->getGpsLocations(locations);
+				// -----------------------------------------------------------------------------
+				forEachXmlChildElementWithTagName(*xmlRootElem, child, "trk")
+				{
+					forEachXmlChildElementWithTagName(*child, subchild, "trkseg")
+						{
+						++segment;
+						forEachXmlChildElementWithTagName(*subchild, subsubchild, "trkpt")
+						{
+							GpsData gpsData;
+
+							++gpsdataid;
+
+							double lat = subsubchild->getDoubleAttribute("lat");
+							double lon = subsubchild->getDoubleAttribute("lon");
+							double ele = (subsubchild->getChildElementAllSubText("ele", "0.0")).getDoubleValue();
+							String trkpttime = subsubchild->getChildElementAllSubText("time", String::empty);
+							++numTrk;
+							int loc = Helper::findLocation(locations, lon, lat);
+
+							gpsData.setGpsData(gpsdataid,
+									(float)lat,
+									(float)lon,
+									trkpttime,
+									ele,
+									segment,
+									m_user,
+									lastFileId,
+									(loc == 0 ? 1: loc));
+
+							gpsDataVec.push_back(gpsData);
+							++countGpxPoints;
+							setProgress(progressNum * numTrk);
+						}
+						}
+				}
+				dbCon->beginTransaction();
+				setStatusMessage("Inserting into database...");
+				setProgress(0.0);
+				int gpsVecSize = gpsDataVec.size();
+				for (int i = 0; i < gpsVecSize; ++i) {
+					if (m_checkDuplicate)
+					{
+						if (!dbCon->checkIfGpsDataExsist(gpsDataVec[i].getTime(), gpsDataVec[i].getUser()))
+						{
+							dbCon->insertGpsData(gpsDataVec[i]);
+						}
+					}
+					else
 					{
 						dbCon->insertGpsData(gpsDataVec[i]);
 					}
+					setProgress( (double)i / gpsVecSize);
 				}
-				else
-				{
-					dbCon->insertGpsData(gpsDataVec[i]);
-				}
-				setProgress( (double)i / gpsVecSize);
+				dbCon->commitTransaction();
 			}
-			dbCon->commitTransaction();
-			m_status = String::empty;
-			m_status << "File imported: " << m_xmlFile.getFileName() << " !";
+			else
+			{
+				m_status = "One or more files already exist! ";
+			}
+			dbCon->closeDbConnection();
 		}
-		else
-		{
-			m_status = "File already exists!";
-		}
-		dbCon->closeDbConnection();
+		m_status += "Imported " + String(countGpxFiles) + " file(s) with " + String(countGpxPoints) + " entries!";
+
+		delete xmlRootElem;
+		delete xmlDoc;
 	}
-	delete xmlRootElem;
-	delete xmlDoc;
-    }
 }
