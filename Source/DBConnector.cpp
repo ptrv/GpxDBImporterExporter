5fcf659b25b39783307b9c55cc138b0341a67f21
ptrv <mail@petervasil.net>
2012-02-27 04:15:59 +0100

    Add DBConnector test class.

--- Source/DBConnector.cpp
+++ Source/DBConnector.cpp
@@ -735,3 +735,36 @@ bool DBConnector::getTableNames(StringArray& names)
 	CATCHDBERRORS
 	return res;
 }
+
+//==============================================================================
+#if UNIT_TESTS
+
+class DBConnectorTest : public UnitTest {
+public:
+	DBConnectorTest() : UnitTest("DBConnector") {}
+
+	void runTest()
+	{
+		String dbPathRelative = "../../../data/testdata/test.db";
+		String dbPath = File::getCurrentWorkingDirectory().getChildFile(dbPathRelative).getFullPathName();
+		DBConnector* db = new DBConnector(dbPath);
+		db->setupDbConnection();
+
+		beginTest("Check if city exists");
+
+		expect(db->checkIfCityExists("Berlin"));
+		expect(! db->checkIfCityExists("foobar"));
+
+		beginTest("Check if file exists");
+
+//		expect(db->checkIfFileExists(""));
+		expect(! db->checkIfFileExists("foobar"));
+
+		db->closeDbConnection();
+		delete db;
+	}
+};
+
+static DBConnectorTest dbConnectorTest;
+
+#endif
