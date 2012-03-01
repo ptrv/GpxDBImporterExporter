7540eff469afaf16583c831b1d453f06de0db528
ptrv <mail@petervasil.net>
2012-03-01 05:43:18 +0100

    Fix linux install script.

--- data/platform/linux/install_icon_and_launcher.sh
+++ data/platform/linux/install_icon_and_launcher.sh
@@ -9,14 +9,14 @@ BIN_PATH=$HOME/bin
 echo "\nGpxDBImporterExporter installer :)\n"
 echo "Install executable..."
 mkdir -p $BIN_PATH
-cp ../../../Builds/Linux/build/GpxDBImporterExporter $BIN_PATH
+cp $BASEDIR/../../../Builds/Linux/build/GpxDBImporterExporter $BIN_PATH
 
 echo "Install launcher..."
 mkdir -p $LAUNCHER_PATH
-cp gpxdbimporterexporter.desktop $LAUNCHER_PATH
+cp $BASEDIR/gpxdbimporterexporter.desktop $LAUNCHER_PATH
 
 echo "Install icon..."
 mkdir -p $ICON_PATH
-cp ../../icon/gpxdbimporterexporter_icon.png $ICON_PATH
+cp $BASEDIR/../../icon/gpxdbimporterexporter_icon.png $ICON_PATH
 
 echo "Done."
